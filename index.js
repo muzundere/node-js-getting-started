611e7c674e8e7ef635fdf6d5e1bff6395ba7d12b
muzundere <42274864+muzundere@users.noreply.github.com>
2020-01-02 02:01:44 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
+// const express = require('express')
+// const path = require('path')
+// const PORT = process.env.PORT || 5000
+
+// express()
+//   .use(express.static(path.join(__dirname, 'public')))
+//   .set('views', path.join(__dirname, 'views'))
+//   .set('view engine', 'ejs')
+//   .post('*', (req, res) => res.render('pages/index'))
+//   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const app = express()
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .post('*', (req, res) => res.render('pages/index'))
+const PORT = process.env.PORT || 8080;
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'));
+}
+// const port = 8080;
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
 
-express().get('*',(req, res) => {
+app.get('*',(req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 });
-express().listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+app.post('*', (req, res) => res.render('pages/index'))
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
