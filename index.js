e50aa60890e28b382aaab8063a31d4aea8b4f00e
muzundere <42274864+muzundere@users.noreply.github.com>
2020-01-02 02:04:21 -0600

    Update index.js

--- index.js
+++ index.js
@@ -10,23 +10,21 @@
 //   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
-const app = express()
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
+const app = express();
 
-const PORT = process.env.PORT || 8080;
 if (process.env.NODE_ENV === 'production') {
 	app.use(express.static('client/build'));
-}
-// const port = 8080;
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs')
+};
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 app.get('*',(req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 });
 
-app.post('*', (req, res) => res.render('pages/index'))
+app.post('*', (req, res) => res.render('pages/index'));
 
-app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
